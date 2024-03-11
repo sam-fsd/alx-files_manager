b11717bebf15dfe2a156ac46d4113910c0c7459f
sam-fsd <kabirusam037@gmail.com>
2024-03-11 22:37:40 +0300

    API endpoints

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import router from './routes/index';
+
+const PORT = parseInt(process.env.PORT, 10) || 5001;
+const app = express();
+
+app.use(express.json());
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log('Server running on port', PORT);
+});
