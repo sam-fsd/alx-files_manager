e9cac8108238413a6f9528af4adfac0353b21ffa
sam-fsd <kabirusam037@gmail.com>
2024-03-13 20:28:05 +0300

    [refactor] getMe method to correctly handle missing token in redis

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,8 +1,11 @@
-import dbClient from '../utils/db';
 import sha1 from 'sha1';
+import { ObjectID } from 'mongodb';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+import { response } from 'express';
 
 class UsersController {
-  static postNew (req, res) {
+  static postNew(req, res) {
     const { email } = req.body;
     const { password } = req.body;
 
@@ -25,13 +28,33 @@ class UsersController {
         users
           .insertOne({
             email,
-            password: hashedPassword
+            password: hashedPassword,
           })
           .then((result) => {
             res.status(201).json({ id: result.insertedId, email });
           });
       }
     });
+  }
+
+  static async getMe(request, response) {
+    const token = request.header('X-Token');
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (userId) {
+      const users = dbClient.db.collection('users');
+      const idObject = new ObjectID(userId);
+      users.findOne({ _id: idObject }, (err, user) => {
+        if (user) {
+          response.status(200).json({ id: userId, email: user.email });
+        } else {
+          response.status(401).json({ error: 'Unauthorized' });
+        }
+      });
+    } else {
+      console.log('Hupatikani!');
+      response.status(401).json({ error: 'Unauthorized' });
+    }
   }
 }
 
